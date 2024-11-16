a456e715f2948eb191ea1f970951ca0e53cb62a5
jifrozen0110 <answldjs1836@gmail.com>
2024-11-16 17:21:40 +0900

    front merge conflicts 수정

--- src/apis/requestBuilder.js
+++ src/apis/requestBuilder.js
@@ -3,7 +3,7 @@ import { getCookie } from "../hooks/cookieUtil";
 
 const { VITE_SERVER_END_POINT, VITE_DEV_SERVER_END_POINT } = import.meta.env;
 
-const SERVER_END_POINT = import.meta.env.DEV ? VITE_DEV_SERVER_END_POINT : VITE_SERVER_END_POINT;
+const SERVER_END_POINT = "http://localhost:8080";
 
 export const request = axios.create({
   baseURL: "http://localhost:8080",
@@ -11,6 +11,12 @@ export const request = axios.create({
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
+export const requestFile = axios.create({
+  baseURL: SERVER_END_POINT,
+  timeout: 10000,
+  headers: { "Content-Type": "multipart/form-data" },
+});
+
 
 export const authRequest = () => {
   return axios.create({
--- src/hooks/cookieUtil.jsx
+++ src/hooks/cookieUtil.jsx
@@ -1,9 +1,10 @@
-﻿import { Cookies } from "react-cookie"
+﻿import { Cookies } from "react-cookie";
 
-const cookies = new Cookies()
+const cookies = new Cookies();
 
-export const setCookie = (name, value, options = {}) => cookies.set(name, value, {path:'/', ...options})
+export const setCookie = (name, value, options = {}) =>
+  cookies.set(name, value, { path: "/", ...options });
 
-export const getCookie = (name) => cookies.get(name)
+export const getCookie = (name) => cookies.get(name);
 
-export const removeCookie = (name) => cookies.remove(name, {path : '/'})
+export const removeCookie = (name) => cookies.remove(name, { path: "/" });
