5daaa7451ebe268d44e8ec8521cc631635a76209
jifrozen0110 <answldjs1836@gmail.com>
2024-11-16 16:54:01 +0900

    home route 변경

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -10,12 +10,11 @@ import ProfilePage from "./ProfilePage";
 export function Routes() {
   return (
     <ReactRouterRoutes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<BattleGame.ListPage />} />
       <Route path="/rank" element={<RankPage />} />
       {/* <Route path="/shop" element={<ShopPage />} /> */}
       {/* <Route path="/options" element={<OptionsPage />} /> */}
       <Route path="/user/:userId" element={<ProfilePage />} />
-      <Route path="/game" element={<BattleGame.ListPage />} />
       <Route path="/game/battle" element={<BattleGame.ListPage />} />
       <Route path="/game/battle/waiting/:roomId" element={<BattleGame.WaitingPage />} />
       <Route path="/game/battle/ingame/:roomId" element={<BattleGame.IngamePage />} />
