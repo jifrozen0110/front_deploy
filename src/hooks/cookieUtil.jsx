a4b4e48735b921cc6c46f8a2ed085db4e2b53783
minjae <minjagot@gmail.com>
2024-12-01 20:59:29 +0900

    fix : cookie domain

--- src/hooks/cookieUtil.jsx
+++ src/hooks/cookieUtil.jsx
@@ -1,4 +1,5 @@
 ﻿import { Cookies } from "react-cookie";
+const domain = import.meta.env.DEV ? "localhost" : "puzzleshare.site";
 
 const cookies = new Cookies();
 
@@ -7,4 +8,4 @@ export const setCookie = (name, value, options = {}) =>
 
 export const getCookie = (name) => cookies.get(name);
 
-export const removeCookie = (name) => cookies.remove(name, { path: "/", domain:"puzzleshare.site" });
+export const removeCookie = (name) => cookies.remove(name, { path: "/", domain: domain});
