cf0bcdfc37ecf9075472d3cf3915aa8542c4ac67
inwoo <jiw413@naver.com>
2024-12-02 14:03:09 +0900

    fix : 친구 모달창 위치 수정

--- src/pages/BattleGame/BattleGameListPage.jsx
+++ src/pages/BattleGame/BattleGameListPage.jsx
@@ -275,7 +275,7 @@ const ModalOverlay = styled.div`
 `;
 
 const ModalContent = styled.div`
-  position: relative;
+  position: fixed;
   background: white;
   border-radius: 8px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
